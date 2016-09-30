f4068addb352de1b15dc7702a764c759248504e5
Parker Smart <Parker.Smart@ncm.com>
2016-09-30 10:44:27 -0600

    Added back the migration.

---
+++ src/SM.DataModels.StuffDataModel.Web/Migrations/20160930164323_migration1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace SM.DataModels.StuffDataModel.Web.Migrations
+{
+    public partial class migration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DateJoined = table.Column<DateTime>(nullable: false),
+                    Email = table.Column<string>(maxLength: 128, nullable: false),
+                    FirstName = table.Column<string>(maxLength: 64, nullable: false),
+                    LastName = table.Column<string>(maxLength: 64, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stuff",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DateAdded = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(maxLength: 64, nullable: false),
+                    ImageUrl = table.Column<string>(maxLength: 256, nullable: false),
+                    Name = table.Column<string>(maxLength: 64, nullable: false),
+                    OwnerId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stuff", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Stuff_People_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "People",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Statuses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DateApproved = table.Column<DateTime>(nullable: true),
+                    DateCheckedIn = table.Column<DateTime>(nullable: true),
+                    DateCheckedOut = table.Column<DateTime>(nullable: true),
+                    DateRequested = table.Column<DateTime>(nullable: true),
+                    RequestorId = table.Column<int>(nullable: true),
+                    StuffId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Statuses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Statuses_People_RequestorId",
+                        column: x => x.RequestorId,
+                        principalTable: "People",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Statuses_Stuff_StuffId",
+                        column: x => x.StuffId,
+                        principalTable: "Stuff",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Statuses_RequestorId",
+                table: "Statuses",
+                column: "RequestorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Statuses_StuffId",
+                table: "Statuses",
+                column: "StuffId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stuff_OwnerId",
+                table: "Stuff",
+                column: "OwnerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Statuses");
+
+            migrationBuilder.DropTable(
+                name: "Stuff");
+
+            migrationBuilder.DropTable(
+                name: "People");
+        }
+    }
+}
