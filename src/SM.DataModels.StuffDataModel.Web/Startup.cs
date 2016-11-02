4dc961f44c6c7ea0d483b8711bdde4f3c87f669b
Parker Smart <Parker.Smart@ncm.com>
2016-11-01 20:09:47 -0600

    Update web page.

--- src/SM.DataModels.StuffDataModel.Web/Startup.cs
+++ src/SM.DataModels.StuffDataModel.Web/Startup.cs
@@ -27,7 +27,7 @@ namespace SM.DataModels.StuffDataModel.Web
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Nothing interesting to see here...");
             });
         }
     }
